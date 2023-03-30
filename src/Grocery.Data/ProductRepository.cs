b513ae11759793920d7e88a0f81b7e81d55bc819
Guzel Ganieva <guzel95@list.ru>
2023-03-30 23:51:58 +0300

    Add project files.

---
+++ src/Grocery.Data/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Grocery.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grocery.Data
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task<IEnumerable<Product>> GetProductsAsync()
+        {
+            List<Product> products = new List<Product>();
+            products.Add(new Product() { Name = "Rise", Id = Guid.NewGuid() });
+            products.Add(new Product() { Name = "Chiken breast", Id = Guid.NewGuid() });
+            return Task.FromResult((IEnumerable<Product>)products);
+        }
+    }
+}
